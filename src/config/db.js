e23b251ce90642ff55e89fc23770174569b2ac5a
Pedro César <pedrocesarfcarneiro@gmail.com>
2025-12-05 15:52:55 -0300

    feat(db): database connection string

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 })
